741d48a427cc76e9c61a298cc8bc2cd8ae12022c
Felipe <santospinheiro627@gmail.com>
2022-03-01 22:19:21 -0300

    add teste2

---
+++ conn/conexão.php
@@ -0,0 +1,6 @@
+<?php
+    define('server', 'localhost');
+    define('user', 'root');
+    define('password', '');
+    define('dbname', 'teste2');
+    $conn = mysqli_connect("server","user", "password","dbname");
