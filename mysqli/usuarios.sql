083c47eacacdd257a20dd45b06a3601d5df5bea4
Felipe pinheiro dos Santos <santospinheiro627@gmail.com>
2022-04-13 10:34:07 -0300

    Update usuarios.sql

--- mysqli/usuarios.sql
+++ mysqli/usuarios.sql
@@ -1,11 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Tempo de geração: 01-Mar-2022 às 23:58
--- Versão do servidor: 5.7.33
--- versão do PHP: 7.4.19
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -13,43 +6,18 @@ SET time_zone = "+00:00";
 
 
 
---
--- Banco de dados: `imports`
---
 
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `usuarios`
---
 
 CREATE TABLE `usuarios` (
-  `id` int(11) NOT NULL,
+  `id`int(11) NOT NULL AUTO_INCREMENT ,
   `nome` varchar(220) NOT NULL,
   `email` varchar(2020) NOT NULL,
   `created` datetime NOT NULL,
   `modified` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Índices para tabelas despejadas
---
 
---
--- Índices para tabela `usuarios`
 --
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT de tabelas despejadas
---
-
---
--- AUTO_INCREMENT de tabela `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
-COMMIT;
-
-/
